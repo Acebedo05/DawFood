5a99f8c9062b642ef7003678ed674905af254c55
acebedo <jaceara2006@g.educaand.es>
2024-01-18 21:54:14 +0100

    Package.daw.tpv (v1 (FuncionesTPV))

--- src/main/java/daw/tpv/FuncionesTPV.java
+++ src/main/java/daw/tpv/FuncionesTPV.java
@@ -1,92 +1,136 @@
 package daw.tpv;
 
-import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.UUID;
+import javax.swing.JOptionPane;
+import java.util.Random;
 
 /**
  *
  * @author acebedo
  */
 public class FuncionesTPV {
-    
-    private String ubicacion;
-    private LocalDateTime fechahoraSistema;
-    public UUID id = UUID.randomUUID();
-    // sout(uuid);
 
-    public FuncionesTPV(String ubicacion, LocalDateTime fechahoraSistema, UUID id) {
-        this.ubicacion = ubicacion;
-        this.fechahoraSistema = fechahoraSistema;
-        this.id = id;
+    private AtributosTPV atributosTPV;
+
+    public FuncionesTPV(AtributosTPV atributosTPV) {
+        this.atributosTPV = atributosTPV;
     }
 
-    public FuncionesTPV() {
-    }
+    // Método para encender el TPV, mostrando un mensaje de bienvenida y permitiendo seleccionar el modo.
+    public void encenderTPV() {
+        String mensajeInicio = "¡Bienvenido al TPV!\n"
+                + "Ubicación del TPV: " + atributosTPV.getUbicacion() + "\n"
+                + "Número de Serie: " + atributosTPV.getNumeroSerie() + "\n"
+                + "Fecha y Hora Actual: " + atributosTPV.getFechaHoraActual();
 
-    public String getUbicacion() {
-        return ubicacion;
-    }
+        JOptionPane.showMessageDialog(null, mensajeInicio);
 
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
+        String opcionSeleccionada = JOptionPane.showInputDialog(
+                null,
+                "Seleccione una opción:\n\n"
+                + "1. Modo Usuario\n"
+                + "2. Modo Administrador"
+        );
 
-    public LocalDateTime getFechahoraSistema() {
-        return fechahoraSistema;
-    }
-
-    public void setFechahoraSistema(LocalDateTime fechahoraSistema) {
-        this.fechahoraSistema = fechahoraSistema;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 53 * hash + Objects.hashCode(this.ubicacion);
-        hash = 53 * hash + Objects.hashCode(this.fechahoraSistema);
-        hash = 53 * hash + Objects.hashCode(this.id);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
+        switch (opcionSeleccionada) {
+            case "Modo Usuario" ->
+                iniciarModoUsuario();
+            case "Modo Administrador" ->
+                iniciarModoAdmin();
+            default ->
+                JOptionPane.showMessageDialog(null, "Opción no válida");
         }
-        if (obj == null) {
+    }
+
+    // Método para iniciar el modo de usuario (CAMBIAR A SOUT).
+    public void iniciarModoUsuario() {
+        JOptionPane.showMessageDialog(null, "Modo Usuario Iniciado");
+    }
+
+    // Método para iniciar el modo de administrador, generando o solicitando la contraseña de administrador.
+    public void iniciarModoAdmin() {
+        String password;
+        if (atributosTPV.getPasswordAdmin() == null || atributosTPV.getPasswordAdmin().isEmpty()) {
+            // Si la contraseña de administrador aún no se ha generado, se crea una nueva.
+            password = generarPasswordAleatoria();
+            atributosTPV.setPasswordAdmin(password);
+            JOptionPane.showMessageDialog(null, "Contraseña de administrador generada: " + password);
+        } else {
+            // Si ya hay una contraseña de administrador creada, se solicita.
+            password = JOptionPane.showInputDialog(null, "Ingrese la contraseña de administrador:");
+        }
+
+        if (password != null && validarPasswordAdmin(password)) {
+            JOptionPane.showMessageDialog(null, "Modo Administrador Iniciado");
+        } else {
+            JOptionPane.showMessageDialog(null, "Contraseña incorrecta. No se puede acceder al modo administrador.");
+        }
+    }
+
+    // Método para realizar una compra (REVISAR).
+    public void realizarCompra() {
+        JOptionPane.showMessageDialog(null, "Compra Realizada");
+    }
+
+    /*
+        Método para validar si la contraseña generada cumple con los requisitos.
+        REQUISITOS:
+            - 1 Letra mayúscula.
+            - 1 Letra minúscula.
+            - 1 Número.
+            - 1 Carácter especial.
+    */
+    private boolean validarPasswordAdmin(String password) {
+        // Validar que la contraseña cumpla con los requisitos.
+        boolean tieneMinuscula = false;
+        boolean tieneMayuscula = false;
+        boolean tieneNumero = false;
+        boolean tieneCaracterEspecial = false;
+
+        for (char caracter : password.toCharArray()) {
+            if (Character.isLowerCase(caracter)) {
+                tieneMinuscula = true;
+            } else if (Character.isUpperCase(caracter)) {
+                tieneMayuscula = true;
+            } else if (Character.isDigit(caracter)) {
+                tieneNumero = true;
+            } else if ("#$%&()*+,-.:;<=>@".indexOf(caracter) != -1) {
+                tieneCaracterEspecial = true;
+            }
+        }
+
+        if (!(tieneMinuscula && tieneMayuscula && tieneNumero && tieneCaracterEspecial)) {
+            JOptionPane.showMessageDialog(null, "La contraseña no cumple con los criterios especificados.");
             return false;
         }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final FuncionesTPV other = (FuncionesTPV) obj;
-        if (!Objects.equals(this.ubicacion, other.ubicacion)) {
-            return false;
-        }
-        if (!Objects.equals(this.fechahoraSistema, other.fechahoraSistema)) {
-            return false;
-        }
-        return Objects.equals(this.id, other.id);
+
+        return true;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("FuncionesTPV{");
-        sb.append("ubicacion=").append(ubicacion);
-        sb.append(", fechahoraSistema=").append(fechahoraSistema);
-        sb.append(", id=").append(id);
-        sb.append('}');
-        return sb.toString();
+    // Método para generar una contraseña aleatoria.
+    private String generarPasswordAleatoria() {
+        // Genera una contraseña aleatoria con los requisitos especificados.
+        Random random = new Random();
+        String password = "";
+
+        // Caracteres permitidos
+        char[] letrasMinusculas = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] letrasMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        char[] numeros = "0123456789".toCharArray();
+        char[] caracteresEspeciales = "#$%&()*+,-.:;<=>@".toCharArray();
+
+        // Añade al menos una minúscula, mayúscula, número y caracter especial.
+        password += letrasMinusculas[random.nextInt(letrasMinusculas.length)];
+        password += letrasMayusculas[random.nextInt(letrasMayusculas.length)];
+        password += numeros[random.nextInt(numeros.length)];
+        password += caracteresEspeciales[random.nextInt(caracteresEspeciales.length)];
+
+        // Completa la contraseña hasta llegar a 6 caracteres.
+        while (password.length() < 6) {
+            int randomIndex = random.nextInt(caracteresEspeciales.length);
+            password += caracteresEspeciales[randomIndex];
+        }
+
+        return password;
     }
-    
+
 }
