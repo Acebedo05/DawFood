5c4b027ab0f88605cfb59f0bb4a4af57714a9c99
acebedo <jaceara2006@g.educaand.es>
2024-01-18 21:55:10 +0100

    Package.daw (v1 Main)

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -1,11 +1,21 @@
 package daw;
 
+import daw.tpv.FuncionesTPV;
+import daw.tpv.ObjetosTPV;
+
 /**
  *
  * @author acebedo
  */
 public class Main {
+
     public static void main(String[] args) {
-      
+
+        // Inicializar el TPV desde la clase ObjetosTPV.
+        FuncionesTPV funcionesTPV = ObjetosTPV.inicializarTPV();
+
+        // Iniciar el TPV.
+        funcionesTPV.encenderTPV();
+
     }
 }
