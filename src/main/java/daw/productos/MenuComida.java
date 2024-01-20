ba9c08d53acd77513ba2b94843abc7d1fc19d975
acebedo <jaceara2006@g.educaand.es>
2024-01-20 21:14:45 +0100

    Package.daw.productos (v1 (Pizza, Hamburguesa, Kebab, MenuComida))

--- src/main/java/daw/productos/MenuComida.java
+++ src/main/java/daw/productos/MenuComida.java
@@ -1,41 +1,186 @@
 package daw.productos;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
- * @author krach
+ * @author acebedo
  */
 public class MenuComida {
 
-    private ArrayList<Comida> comidas;
+    // Listas para almacenar las diferentes categorías de comidas.
+    private List<Pizza> pizzas;
+    private List<Hamburguesa> hamburguesas;
+    private List<Kebab> kebabs;
 
-    public void MenuComidas() {
-        comidas = new ArrayList<>();
-        inicializarComidas();
+    // Constructor de la clase, se inicializan las listas y se llama al método principal.
+    public void MenuComida() {
+        this.pizzas = new ArrayList<>();
+        this.hamburguesas = new ArrayList<>();
+        this.kebabs = new ArrayList<>();
 
+        // Inicializar las listas.
+        inicializarPizzas();
+        inicializarHamburguesas();
+        inicializarKebabs();
+
+        // Mostrar el menú principal de comidas.
+        menuSeleccionComidas();
     }
 
-    private void inicializarComidas() {
-
-        comidas.add(new Pizza("P001", "4 Quesos", true, "Comidas", "Pizzas", "Pizza hecha de 4 quesos distintos", 12, 0.10));
-        comidas.add(new Pizza("P002", "Margarita", true, "Comidas", "Pizzas", "Pizza basica", 10, 0.10));
-        comidas.add(new Pizza("P003", "Boloñesa", true, "Comidas", "Pizzas", "Pizza hecha de queso y  carne", 15, 0.10));
-
-        comidas.add(new Hamburguesa("H001", "Pollo", true, "Comidas", "Hamburguesa", "Hamburguesa de pollo", 6, 0.10));
-        comidas.add(new Hamburguesa("H002", "Ternera", true, "Comidas", "Hamburguesa", "Hamburguesa de ternera", 10, 0.10));
-        comidas.add(new Hamburguesa("H003", "Vegetal", true, "Comidas", "Hamburguesa", "Hamburguesa vegetal", 12, 0.10));
-
-        comidas.add(new Kebab("K001", "Pollo", true, "Comidas", "Kebab", "Kebab de pollo", 4, 0.10));
-        comidas.add(new Kebab("K002", "Ternera", true, "Comidas", "Kebab", "Kebab de ternera", 4.50, 0.10));
-        comidas.add(new Kebab("K003", "Mixto", true, "Comidas", "Kebab", "Kebab mixto", 5.50, 0.10));
-
+    // Inicializar la lista de pizzas con datos.
+    private void inicializarPizzas() {
+        pizzas.add(new Pizza("P01", "Queso", 8.99, true, "Pizza con cuatro tipos de quesos", "Comida", 0.10, "Pizza"));
+        pizzas.add(new Pizza("P02", "Margarita", 9.99, true, "Pizza con queso y tomate", "Comida", 0.10, "Pizza"));
+        pizzas.add(new Pizza("P03", "Boloñesa", 10.99, true, "Pizza con salsa boloñesa", "Comida", 0.10, "Pizza"));
     }
 
-//    public void mostrarMenu() {
-//        JOptionPane.showConfirmDialog(null, hola());
-//        Por hacer
-//    }
+    // Inicializar la lista de hamburguesas con datos.
+    private void inicializarHamburguesas() {
+        hamburguesas.add(new Hamburguesa("H01", "Ternera", 7.99, true, "Hamburguesa de ternera", "Comida", 0.10, "Hamburguesa"));
+        hamburguesas.add(new Hamburguesa("H02", "Pollo", 6.99, true, "Hamburguesa de pollo", "Comida", 0.10, "Hamburguesa"));
+        hamburguesas.add(new Hamburguesa("H03", "Vegetal", 5.99, true, "Hamburguesa vegetariana", "Comida", 0.10, "Hamburguesa"));
+    }
+
+    // Inicializar la lista de kebabs con datos.
+    private void inicializarKebabs() {
+        kebabs.add(new Kebab("K01", "Ternera", 9.99, true, "Kebab de ternera", "Comida", 0.10, "Kebab"));
+        kebabs.add(new Kebab("K02", "Pollo", 8.99, true, "Kebab de pollo", "Comida", 0.10, "Kebab"));
+        kebabs.add(new Kebab("K03", "Mixto", 10.99, true, "Kebab mixto", "Comida", 0.10, "Kebab"));
+    }
+
+    // Método principal que muestra el menú de selección para el usuario.
+    public void menuSeleccionComidas() {
+        // Crear la ventana del menú.
+        JFrame frame = new JFrame("Tipos de Comida");
+
+        // Crear un panel para colocar los botones.
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // Crear botones para cada opción del menú.
+        // Asociar ActionListener a cada botón.
+        agregarBoton(panel, "Pizzas", e -> {
+            menuSeleccionPizzas();
+            frame.dispose();
+        });
+        agregarBoton(panel, "Hamburguesas", e -> {
+            menuSeleccionHamburguesas();
+            frame.dispose();
+        });
+        agregarBoton(panel, "Kebabs", e -> {
+            menuSeleccionKebabs();
+            frame.dispose();
+        });
+        agregarBoton(panel, "Volver al menú de selección", e -> {
+            hola();
+            frame.dispose();
+        });
+        agregarBoton(panel, "No Comprar", e -> {
+            hola();
+            frame.dispose();
+        });
+        agregarBoton(panel, "Ver carrito", e -> {
+            hola();
+            frame.dispose();
+        });
+
+        frame.add(panel);
+        frame.setSize(400, 130);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    // Método para agregar un botón al panel
+    private void agregarBoton(JPanel panel, String texto, ActionListener actionListener) {
+        // Crear botones para cada opción del menú.
+        // Asociar ActionListener a cada botón.
+        JButton boton = new JButton(texto);
+        boton.addActionListener(actionListener);
+        // Agregar botones al panel.
+        panel.add(boton);
+    }
+
+    // Método principal que muestra el menú de pizzas al usuario.
+    public void menuSeleccionPizzas() {
+        // Crear la ventana del menú.
+        JFrame frame = new JFrame("Tipos de Pizzas");
+
+        // Crear un panel para colocar los botones.
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // Crear botones para cada opción del menú.
+        // Asociar ActionListener a cada botón.
+        agregarBoton(panel, "4 Quesos", e -> hola());
+        agregarBoton(panel, "Margarita", e -> hola());
+        agregarBoton(panel, "Boloñesa", e -> hola());
+        agregarBoton(panel, "Volver al menú de selección", e -> hola());
+        agregarBoton(panel, "No Comprar", e -> hola());
+        agregarBoton(panel, "Ver carrito", e -> hola());
+
+        frame.add(panel);
+        frame.setSize(400, 130);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    // Método principal que muestra el menú de hamburguesas al usuario.
+    public void menuSeleccionHamburguesas() {
+        // Crear la ventana del menú.
+        JFrame frame = new JFrame("Tipos de Hamburguesas");
+
+        // Crear un panel para colocar los botones.
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // Crear botones para cada opción del menú.
+        // Asociar ActionListener a cada botón.
+        agregarBoton(panel, "Ternera", e -> hola());
+        agregarBoton(panel, "Pollo", e -> hola());
+        agregarBoton(panel, "Vegetariana", e -> hola());
+        agregarBoton(panel, "Volver al menú de selección", e -> hola());
+        agregarBoton(panel, "No Comprar", e -> hola());
+        agregarBoton(panel, "Ver carrito", e -> hola());
+
+        frame.add(panel);
+        frame.setSize(400, 130);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    // Método principal que muestra el menú de kebabs al usuario.
+    public void menuSeleccionKebabs() {
+        // Crear la ventana del menú.
+        JFrame frame = new JFrame("Tipos de Kebabs");
+
+        // Crear un panel para colocar los botones.
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // Crear botones para cada opción del menú.
+        // Asociar ActionListener a cada botón.
+        agregarBoton(panel, "Ternera", e -> hola());
+        agregarBoton(panel, "Pollo", e -> hola());
+        agregarBoton(panel, "Mixto", e -> hola());
+        agregarBoton(panel, "Volver al menú de selección", e -> hola());
+        agregarBoton(panel, "No Comprar", e -> hola());
+        agregarBoton(panel, "Ver carrito", e -> hola());
+
+        frame.add(panel);
+        frame.setSize(400, 130);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void hola() {
+        System.out.println("hola");
+    }
 
 }
