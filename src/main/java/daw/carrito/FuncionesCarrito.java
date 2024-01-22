8ab1501578e6d6b9dbfd7e0ffa53eaf0b2886b8f
acebedo <jaceara2006@g.educaand.es>
2024-01-22 20:11:10 +0100

    Package.daw.carrito (v3 (FuncionesCarrito))

--- src/main/java/daw/carrito/FuncionesCarrito.java
+++ src/main/java/daw/carrito/FuncionesCarrito.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class FuncionesCarrito {
 
     private static List<Producto> carrito = new ArrayList<>();
-    ;
+    
     private FuncionesUsuario funcionesUsuario;
 
     public FuncionesCarrito(FuncionesUsuario funcionesUsuario) {
